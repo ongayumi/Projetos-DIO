761528d2b2c473046a1cc6fc31ee9d2b1156ed43
Evelyn Ayumi Onga <ongayumi@gmail.com>
2022-07-04 10:00:11 -0300

    javascript-mod3 contador e to-do list

---
+++ Javascript-ModIII/teste.js
@@ -0,0 +1,21 @@
+// var y = 0;
+/*var x=2;
+
+dgtjtgfdf
+fhdrhfd */
+// console.log('Hello world!');
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++){
+        if (array[i] % 2 === 0) {
+            evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]} nao e par!`);
+        }
+    }   
+    console.log("os numeros pares sao:",evenNums) ; 
+}
+
+let array = [1, 2, 4, 5, 7, 8];
+returnEvenValues(array);
